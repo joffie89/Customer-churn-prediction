d0395494e2f4ead97d863570ccaff60bc14b3aeb
Joffie J <54671870+joffie89@users.noreply.github.com>
2024-07-31 13:04:34 +0100

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,1 +1,17 @@
+import joblib
+from flask import Flask, request, jsonify
 
+# Load the best model
+best_model = joblib.load('models/churn_model.pkl')
+
+# Create Flask app
+app = Flask(__name__)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json(force=True)
+    prediction = best_model.predict([list(data.values())])
+    return jsonify({'prediction': int(prediction[0])})
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
